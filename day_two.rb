38269b7f36f22e89530966044f1062cbb137efa9
biannetta <benjamin.iannetta@gmail.com>
2023-12-14 21:37:40 -0500

    Advent Code Second Star

--- day_two.rb
+++ day_two.rb
@@ -6,24 +6,44 @@ CubeCounts = {
 
 $validGames = []
 $invalidGames = [] 
+$totalGames = []
 
 File.open('day_two_input.txt').each do | line |
   gameCount = line.split(':')[0][/\d+/].to_i
   games = line.split(':')[1]
   $validGames.push(gameCount)
 
+  draws = { "red" => 0, "blue" => 0, "green" => 0 }
+
   games.split(';').each do | game |
     game.split(',').each do | draw |
       currentDrawColour = draw[/(blue|green|red)/]
       currentDrawCount = draw[/\d+/].to_i
+
+      if draws[currentDrawColour] < currentDrawCount
+        draws[currentDrawColour] = currentDrawCount
+      end
+      
       if $validGames.include?(gameCount) && currentDrawCount > CubeCounts[currentDrawColour]
         $invalidGames.push(gameCount)
         $validGames.delete(gameCount)
       end
+
     end
   end
+
+  $totalGames.push(draws)
+
 end
 
-p $invalidGames
-p $validGames
 p $validGames.sum
+
+sum = 0
+$totalGames.each { | game |
+  product = 1
+  game.each { | key, value |
+    product *= value
+  }
+  sum += product
+}
+p sum
